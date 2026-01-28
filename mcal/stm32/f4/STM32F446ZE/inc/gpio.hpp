a7e55625d2277911328e6282f56d0c7e1ffb18a1
Jens Böckmann <jensboe@outlook.de>
2026-01-28 22:39:27 +0100

    Use all LEDs to blink

--- mcal/stm32/f4/STM32F446ZE/inc/gpio.hpp
+++ mcal/stm32/f4/STM32F446ZE/inc/gpio.hpp
@@ -1,6 +1,6 @@
 #include "concept/gpio.hpp"
 #include "stm32f4xx.h"
-template <uint32_t GPIO_BASE>
+template <uint32_t GPIO_BASE, uint32_t RCC_AHB1ENR_GPIOxEN>
 struct stm32f4gpio
 {
 private:
@@ -10,7 +10,7 @@ private:
     }
 
 public:
-    static void enableClock() { RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN; }
+    static void enableClock() { RCC->AHB1ENR |= RCC_AHB1ENR_GPIOxEN; }
     static void setModeOutput(uint8_t pin)
     {
         GPIOB->MODER &= ~(0b11 << (pin * 2));
@@ -20,7 +20,15 @@ public:
     static void writeLow(uint8_t pin) { GPIOB->ODR &= ~(1 << pin); }
 };
 
-using GpioB = stm32f4gpio<GPIOB_BASE>;
+using GpioA = stm32f4gpio<GPIOA_BASE, RCC_AHB1ENR_GPIOAEN>;
+using GpioB = stm32f4gpio<GPIOB_BASE, RCC_AHB1ENR_GPIOBEN>;
+using GpioC = stm32f4gpio<GPIOC_BASE, RCC_AHB1ENR_GPIOCEN>;
+using GpioD = stm32f4gpio<GPIOD_BASE, RCC_AHB1ENR_GPIODEN>;
+using GpioE = stm32f4gpio<GPIOE_BASE, RCC_AHB1ENR_GPIOEEN>;
+using GpioF = stm32f4gpio<GPIOF_BASE, RCC_AHB1ENR_GPIOFEN>;
+using GpioG = stm32f4gpio<GPIOG_BASE, RCC_AHB1ENR_GPIOGEN>;
+using GpioH = stm32f4gpio<GPIOH_BASE, RCC_AHB1ENR_GPIOHEN>;
+
 
 template <GpioPort Port, uint8_t Pin>
 struct GpioPin
--- projects/nucleof446ze/blinky/src/main.cpp
+++ projects/nucleof446ze/blinky/src/main.cpp
@@ -2,16 +2,24 @@
 #include "gpio.hpp"
 
 using LD_Green = GpioPin<GpioB, 0>;
+using LD_Blue  = GpioPin<GpioB, 7>;
+using LD_Red   = GpioPin<GpioB, 14>; 
 
 int main(void)
 {
     LD_Green::init();
+    LD_Blue::init();
+    LD_Red::init();
 
     while (1)
     {
         LD_Green::set();
+        LD_Blue::set();
+        LD_Red::set();
         LD_Green::clear();
-    }
+        LD_Blue::clear();
+        LD_Red::clear();
+    } 
 
     return 0;
 }
