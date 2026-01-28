b9d72261321b1b73c782efa801640399c73d094c
Jens Böckmann <jensboe@outlook.de>
2026-01-28 22:34:39 +0100

    Cleaning cmake

---
+++ cmake/global.cmake
@@ -0,0 +1,17 @@
+add_compile_options(
+    -Wall
+    -Wextra
+    -Wpedantic
+    -fdata-sections
+    -ffunction-sections
+    -Werror
+)
+if(CMAKE_BUILD_TYPE MATCHES Debug)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O0 -g3")
+endif()
+if(CMAKE_BUILD_TYPE MATCHES Release)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Os -g0")
+endif()
+
+
+set(CMAKE_CXX_FLAGS "${CMAKE_C_FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
