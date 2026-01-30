52a80396e93586ad5b183e6c7ce2414b0f2b7cc8
Jens Böckmann <jensboe@outlook.de>
2026-01-30 16:34:29 +0100

    Expand port definition to GpioK which seems to be max for STM32F4

--- mcal/stm32/f4/inc/gpio.hpp
+++ mcal/stm32/f4/inc/gpio.hpp
@@ -39,24 +39,50 @@ struct GpioImpl
         return reinterpret_cast<GPIO_TypeDef *>(GPIO_BASE)->IDR & (1 << pin);
     }
 };
-
+#ifdef GPIOA_BASE
 using GpioA = GpioImpl<GPIOA_BASE, RCC_AHB1ENR_GPIOAEN>;
-using GpioB = GpioImpl<GPIOB_BASE, RCC_AHB1ENR_GPIOBEN>;
-using GpioC = GpioImpl<GPIOC_BASE, RCC_AHB1ENR_GPIOCEN>;
-using GpioD = GpioImpl<GPIOD_BASE, RCC_AHB1ENR_GPIODEN>;
-using GpioE = GpioImpl<GPIOE_BASE, RCC_AHB1ENR_GPIOEEN>;
-using GpioF = GpioImpl<GPIOF_BASE, RCC_AHB1ENR_GPIOFEN>;
-using GpioG = GpioImpl<GPIOG_BASE, RCC_AHB1ENR_GPIOGEN>;
-using GpioH = GpioImpl<GPIOH_BASE, RCC_AHB1ENR_GPIOHEN>;
-
 static_assert(GpioPort<GpioA>);
+#endif
+#ifdef GPIOB_BASE
+using GpioB = GpioImpl<GPIOB_BASE, RCC_AHB1ENR_GPIOBEN>;
 static_assert(GpioPort<GpioB>);
+#endif
+#ifdef GPIOC_BASE
+using GpioC = GpioImpl<GPIOC_BASE, RCC_AHB1ENR_GPIOCEN>;
 static_assert(GpioPort<GpioC>);
+#endif
+#ifdef GPIOD_BASE
+using GpioD = GpioImpl<GPIOD_BASE, RCC_AHB1ENR_GPIODEN>;
 static_assert(GpioPort<GpioD>);
+#endif
+#ifdef GPIOE_BASE
+using GpioE = GpioImpl<GPIOE_BASE, RCC_AHB1ENR_GPIOEEN>;
 static_assert(GpioPort<GpioE>);
+#endif
+#ifdef GPIOF_BASE
+using GpioF = GpioImpl<GPIOF_BASE, RCC_AHB1ENR_GPIOFEN>;
 static_assert(GpioPort<GpioF>);
+#endif
+#ifdef GPIOG_BASE
+using GpioG = GpioImpl<GPIOG_BASE, RCC_AHB1ENR_GPIOGEN>;
 static_assert(GpioPort<GpioG>);
+#endif
+#ifdef GPIOH_BASE
+using GpioH = GpioImpl<GPIOH_BASE, RCC_AHB1ENR_GPIOHEN>;
 static_assert(GpioPort<GpioH>);
+#endif
+#ifdef GPIOI_BASE
+using GpioI = GpioImpl<GPIOI_BASE, RCC_AHB1ENR_GPIOIEN>;
+static_assert(GpioPort<GpioH>);
+#endif
+#ifdef GPIOJ_BASE
+using GpioJ = GpioImpl<GPIOJ_BASE, RCC_AHB1ENR_GPIOJEN>;
+static_assert(GpioPort<GpioJ>);
+#endif
+#ifdef GPIOK_BASE
+using GpioK = GpioImpl<GPIOK_BASE, RCC_AHB1ENR_GPIOKEN>;
+static_assert(GpioPort<GpioK>);
+#endif
 
 template <GpioPort Port, uint8_t Pin, GpioPinMode Mode = GpioPinMode::Input>
 struct GpioPin
