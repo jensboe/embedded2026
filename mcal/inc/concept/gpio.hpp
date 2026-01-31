94b894a3861c5c5a70c9007702bccf446d2c9c56
Jens Böckmann <jensboe@outlook.de>
2026-01-31 13:22:59 +0100

    Add clang format

--- mcal/inc/concept/gpio.hpp
+++ mcal/inc/concept/gpio.hpp
@@ -3,16 +3,16 @@
 
 enum class GpioPinMode
 {
-    Input,
-    Output,
+	Input,
+	Output,
 };
 
 template <typename T>
 concept GpioPort = requires(T t, uint8_t pin, GpioPinMode mode) {
-    { T::enable() } -> std::same_as<void>;
-    { T::disable() } -> std::same_as<void>;
-    { T::template setMode<5, GpioPinMode::Output>() } -> std::same_as<void>;
-    { T::template writeHigh<5>() } -> std::same_as<void>;
-    { T::template writeLow<5>() } -> std::same_as<void>;
-    { T::template read<5>() } -> std::same_as<bool>;
+	{ T::enable() } -> std::same_as<void>;
+	{ T::disable() } -> std::same_as<void>;
+	{ T::template setMode<5, GpioPinMode::Output>() } -> std::same_as<void>;
+	{ T::template writeHigh<5>() } -> std::same_as<void>;
+	{ T::template writeLow<5>() } -> std::same_as<void>;
+	{ T::template read<5>() } -> std::same_as<bool>;
 };
