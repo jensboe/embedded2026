0ab6ff13bfda21c090392d58cb1b9ff53abae11f
Jens <jensboe@outlook.de>
2026-01-31 15:44:13 +0100

    Add feature: Delays (#8)
    * Add pragma once
    
    * Implement a data watchpoint and trace based delay function
    
    * Add namespaces

--- mcal/inc/concept/gpio.hpp
+++ mcal/inc/concept/gpio.hpp
@@ -1,18 +1,21 @@
+#pragma once
 #include <concepts>
 #include <cstdint>
-
-enum class GpioPinMode
+namespace mcal::concepts
 {
-	Input,
-	Output,
-};
+	enum class GpioPinMode
+	{
+		Input,
+		Output,
+	};
 
-template <typename T>
-concept GpioPort = requires(T t, uint8_t pin, GpioPinMode mode) {
-	{ T::enable() } -> std::same_as<void>;
-	{ T::disable() } -> std::same_as<void>;
-	{ T::template setMode<5, GpioPinMode::Output>() } -> std::same_as<void>;
-	{ T::template writeHigh<5>() } -> std::same_as<void>;
-	{ T::template writeLow<5>() } -> std::same_as<void>;
-	{ T::template read<5>() } -> std::same_as<bool>;
-};
+	template <typename T>
+	concept GpioPort = requires(T t, uint8_t pin, GpioPinMode mode) {
+		{ T::enable() } -> std::same_as<void>;
+		{ T::disable() } -> std::same_as<void>;
+		{ T::template setMode<5, GpioPinMode::Output>() } -> std::same_as<void>;
+		{ T::template writeHigh<5>() } -> std::same_as<void>;
+		{ T::template writeLow<5>() } -> std::same_as<void>;
+		{ T::template read<5>() } -> std::same_as<bool>;
+	};
+} // namespace mcal::concepts
--- projects/nucleof446ze/blinky/src/main.cpp
+++ projects/nucleof446ze/blinky/src/main.cpp
@@ -1,9 +1,12 @@
+#include "delay.hpp"
 #include "gpio.hpp"
-#include <cstdint>
+
+using namespace mcal::stm32::f4;
 
 using LD_Green = GpioPin<GpioB, 0, GpioPinMode::Output>;
 using LD_Blue = GpioPin<GpioB, 7, GpioPinMode::Output>;
 using LD_Red = GpioPin<GpioB, 14, GpioPinMode::Output>;
+using Delay = DelayImpl<16'000'000>;
 
 int main(void)
 {
@@ -16,9 +19,11 @@ int main(void)
 		LD_Green::set();
 		LD_Blue::set();
 		LD_Red::set();
+		Delay::ms(500);
 		LD_Green::clear();
 		LD_Blue::clear();
 		LD_Red::clear();
+		Delay::ms(500);
 	}
 
 	return 0;
