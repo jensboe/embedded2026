71515aa22defd3be4d6a3585743589deae53a8db
Jens Böckmann <jensboe@outlook.de>
2026-02-08 17:21:35 +0100

    Add rtos test target

---
+++ projects/rtos-blinky/src/main.cpp
@@ -0,0 +1,61 @@
+/**
+ * @file main.cpp
+ * @brief Simple blinky application for Nucleo-F446ZE using GPIO and DWT delay.
+ */
+
+#include "bsp.h"
+#include "mcal.hpp"
+#include "utils.hpp"
+#include <FreeRTOS.h>
+#include <cstdio>
+#include <task.h>
+/**
+ * @brief Use the Nucleo F446ZE board with 100 MHz system clock.
+ */
+using board = bsp::nucleo_f446ze<100 * utils::unit::MHz>;
+
+/*-----------------------------------------------------------*/
+
+static void blink_blue(void *parameters)
+{
+	(void)parameters;
+	for (;;)
+	{
+		board::LD_Blue::set();
+		vTaskDelay(100);
+		board::LD_Blue::clear();
+		vTaskDelay(100);
+	}
+}
+
+static void blink_green(void *parameters)
+{
+	(void)parameters;
+
+	for (;;)
+	{
+		board::LD_Green::set();
+		vTaskDelay(104);
+		board::LD_Green::clear();
+		vTaskDelay(104);
+	}
+}
+
+/**
+ * @brief Main entry point.
+ */
+int main() noexcept
+{
+	board::init();
+	static StaticTask_t blueTaskTCB;
+	static StackType_t blueTaskStack[configMINIMAL_STACK_SIZE];
+	static StaticTask_t greenTaskTCB;
+	static StackType_t greenTaskStack[configMINIMAL_STACK_SIZE];
+
+	(void)xTaskCreateStatic(blink_blue, "blue", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 1U,
+							&(blueTaskStack[0]), &(blueTaskTCB));
+	(void)xTaskCreateStatic(blink_green, "green", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 1U,
+							&(greenTaskStack[0]), &(greenTaskTCB));
+	vTaskStartScheduler();
+	return 0;
+}
